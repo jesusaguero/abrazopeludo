460ad791de4ddab186e79eaf79ac70e40e184549
jesusaguerob <jesus.aguero@tecsup.edu.pe>
2023-06-30 06:41:45 -0500

    update_foros

--- resources/views/foros.blade.php
+++ resources/views/foros.blade.php
@@ -66,25 +66,23 @@
     <!-- Navbar End -->
 
 
-    <!-- Blog Start -->
+<!-- SECCIÓN DE FOROS -->
     <div class="container pt-5">
         <div class="d-flex flex-column text-center mb-5 pt-5">
-            <h4 class="text-secondary mb-3">Pet Blog</h4>
-            <h1 class="display-4 m-0"><span class="text-primary">Updates</span> From Blog</h1>
+            <h4 class="text-secondary mb-3">AbrazoPeludo Blog</h4>
+            <h1 class="display-4 m-0"><span class="text-primary">Conoce más</span> de nuestros engreídos</h1>
         </div>
         <div class="row pb-3">
             <div class="col-lg-4 mb-4">
                 <div class="card border-0 mb-2">
                     <img class="card-img-top" src="import/img/blog-1.jpg" alt="">
                     <div class="card-body bg-light p-4">
-                        <h4 class="card-title text-truncate">Diam amet eos at no eos</h4>
+                        <h4 class="card-title text-truncate">Ben encuentra su hogar</h4>
                         <div class="d-flex mb-3">
-                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Admin</small>
-                            <small class="mr-2"><i class="fa fa-folder text-muted"></i> Web Design</small>
-                            <small class="mr-2"><i class="fa fa-comments text-muted"></i> 15</small>
+                            <small class="mr-2"><i class="fa fa-user text-muted"></i>  Jesus Agüero</small>
                         </div>
-                        <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo</p>
-                        <a class="font-weight-bold" href="">Read More</a>
+                        <p>Después de meses en un refugio, Ben, un perro de raza mixta, finalmente encontró su hogar para siempre. Ahora, disfruta de largos paseos y de una familia amorosa que le brinda todo el cariño que se merece.</p>
+                        <a class="font-weight-bold" href="">Historias que nos dan vida.</a>
                     </div>
                 </div>
             </div>
@@ -92,14 +90,12 @@
                 <div class="card border-0 mb-2">
                     <img class="card-img-top" src="import/img/blog-2.jpg" alt="">
                     <div class="card-body bg-light p-4">
-                        <h4 class="card-title text-truncate">Diam amet eos at no eos</h4>
+                        <h4 class="card-title text-truncate">Una segunda oportunidad</h4>
                         <div class="d-flex mb-3">
-                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Admin</small>
-                            <small class="mr-2"><i class="fa fa-folder text-muted"></i> Web Design</small>
-                            <small class="mr-2"><i class="fa fa-comments text-muted"></i> 15</small>
+                            <small class="mr-2"><i class="fa fa-user text-muted"></i>  Bruno Chigne</small>
                         </div>
-                        <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo</p>
-                        <a class="font-weight-bold" href="">Read More</a>
+                        <p>Luna, una hermosa gata negra, fue adoptada recientemente por una pareja. Después de una breve adaptación, Luna ha demostrado ser una compañera leal y juguetona, llenando de alegría la vida de sus nuevos dueños.</p>
+                        <a class="font-weight-bold" href="">Historias que nos dan vida.</a>
                     </div>
                 </div>
             </div>
@@ -107,84 +103,57 @@
                 <div class="card border-0 mb-2">
                     <img class="card-img-top" src="import/img/blog-3.jpg" alt="">
                     <div class="card-body bg-light p-4">
-                        <h4 class="card-title text-truncate">Diam amet eos at no eos</h4>
+                        <h4 class="card-title text-truncate">Rescate exitoso</h4>
                         <div class="d-flex mb-3">
-                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Admin</small>
-                            <small class="mr-2"><i class="fa fa-folder text-muted"></i> Web Design</small>
-                            <small class="mr-2"><i class="fa fa-comments text-muted"></i> 15</small>
+                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Edilson Huaman</small>
                         </div>
-                        <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo</p>
-                        <a class="font-weight-bold" href="">Read More</a>
+                        <p>Toby, un encantador perro de raza mixta, fue rescatado de la calle y ahora tiene una nueva familia. Su historia de superación y su energía inagotable han cautivado a sus adoptantes, quienes no podrían estar más felices.</p>
+                        <a class="font-weight-bold" href="">Historias que nos dan vida.</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 mb-4">
                 <div class="card border-0 mb-2">
-                    <img class="card-img-top" src="import/img/blog-2.jpg" alt="">
+                    <img class="card-img-top" src="import/img/blog-4.jpg" alt="">
                     <div class="card-body bg-light p-4">
-                        <h4 class="card-title text-truncate">Diam amet eos at no eos</h4>
+                        <h4 class="card-title text-truncate">La dulce historia</h4>
                         <div class="d-flex mb-3">
-                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Admin</small>
-                            <small class="mr-2"><i class="fa fa-folder text-muted"></i> Web Design</small>
-                            <small class="mr-2"><i class="fa fa-comments text-muted"></i> 15</small>
+                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Jesus Agüero </small>
                         </div>
-                        <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo</p>
-                        <a class="font-weight-bold" href="">Read More</a>
+                        <p>Bella, una perrita abandonada, ha sido adoptada por una niña de ocho años. Desde el primer momento, han sido inseparables, formando un vínculo inquebrantable. Ahora, Bella es parte integral de la vida de esta valiente niña.</p>
+                        <a class="font-weight-bold" href="">Historias que nos dan vida.</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 mb-4">
                 <div class="card border-0 mb-2">
-                    <img class="card-img-top" src="import/img/blog-3.jpg" alt="">
+                    <img class="card-img-top" src="import/img/blog-5.jpg" alt="">
                     <div class="card-body bg-light p-4">
-                        <h4 class="card-title text-truncate">Diam amet eos at no eos</h4>
+                        <h4 class="card-title text-truncate">Una nueva vida</h4>
                         <div class="d-flex mb-3">
-                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Admin</small>
-                            <small class="mr-2"><i class="fa fa-folder text-muted"></i> Web Design</small>
-                            <small class="mr-2"><i class="fa fa-comments text-muted"></i> 15</small>
+                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Bruno Chigne</small>
                         </div>
-                        <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo</p>
-                        <a class="font-weight-bold" href="">Read More</a>
+                        <p>Max, un perro de raza Labrador, fue encontrado vagando solo en las calles. Afortunadamente, un amante de los animales decidió adoptarlo. Ahora, Max disfruta de un hogar cálido y seguro, retribuyendo el amor con su lealtad incondicional.</p>
+                        <a class="font-weight-bold" href="">Historias que nos dan vida.</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 mb-4">
                 <div class="card border-0 mb-2">
-                    <img class="card-img-top" src="import/img/blog-1.jpg" alt="">
+                    <img class="card-img-top" src="import/img/blog-6.jpg" alt="">
                     <div class="card-body bg-light p-4">
-                        <h4 class="card-title text-truncate">Diam amet eos at no eos</h4>
+                        <h4 class="card-title text-truncate">Encanto felino</h4>
                         <div class="d-flex mb-3">
-                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Admin</small>
-                            <small class="mr-2"><i class="fa fa-folder text-muted"></i> Web Design</small>
-                            <small class="mr-2"><i class="fa fa-comments text-muted"></i> 15</small>
+                            <small class="mr-2"><i class="fa fa-user text-muted"></i> Edilson Huaman</small>
                         </div>
-                        <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo</p>
-                        <a class="font-weight-bold" href="">Read More</a>
+                        <p>Oliver, un juguetón gatito atigrado, ha sido adoptado por una familia que siempre ha sido amante de los felinos. Su personalidad traviesa y sus ronroneos reconfortantes han conquistado el corazón de sus nuevos dueños.</p>
+                        <a class="font-weight-bold" href="">Historias que nos dan vida.</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-12">
-                <nav aria-label="Page navigation">
-                  <ul class="pagination justify-content-center mb-4">
-                    <li class="page-item disabled">
-                      <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo; Previous</span>
-                      </a>
-                    </li>
-                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">Next &raquo;</span>
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
             </div>
         </div>
     </div>
-    <!-- Blog End -->
+<!-- SECCIÓN DE FOROS -->
 
 
     <!-- Footer Start -->
