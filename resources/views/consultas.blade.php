f3c7f991c3f5d400fb4f0ec796b8f7503dfdae28
jesusaguerob <jesus.aguero@tecsup.edu.pe>
2023-06-30 06:40:55 -0500

    update_foros

--- resources/views/consultas.blade.php
+++ resources/views/consultas.blade.php
@@ -66,45 +66,85 @@
     <!-- Navbar End -->
 
 
-    <!-- Contact Start -->
-    <div class="container-fluid pt-5">
-        <div class="d-flex flex-column text-center mb-5 pt-5">
-            <h4 class="text-secondary mb-3">Contact Us</h4>
-            <h1 class="display-4 m-0">Contact For <span class="text-primary">Any Query</span></h1>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-12 col-sm-8 mb-5">
-                <div class="contact-form">
-                    <div id="success"></div>
-                    <form name="sentMessage" id="contactForm" novalidate="novalidate">
-                        <div class="control-group">
-                            <input type="text" class="form-control p-4" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
-                            <p class="help-block text-danger"></p>
-                        </div>
-                        <div class="control-group">
-                            <input type="email" class="form-control p-4" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
-                            <p class="help-block text-danger"></p>
-                        </div>
-                        <div class="control-group">
-                            <input type="text" class="form-control p-4" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
-                            <p class="help-block text-danger"></p>
-                        </div>
-                        <div class="control-group">
-                            <textarea class="form-control p-4" rows="6" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
-                            <p class="help-block text-danger"></p>
-                        </div>
-                        <div>
-                            <button class="btn btn-primary py-3 px-5" type="submit" id="sendMessageButton">Send Message</button>
-                        </div>
-                    </form>
+    <!-- SECCIÓN DE CONSULTAS -->
+    <div class="accordion" id="preguntas-accordion">
+        <div class="card">
+            <div class="card-header" id="pregunta-1">
+                <h2 class="mb-0">
+                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#respuesta-1" aria-expanded="true" aria-controls="respuesta-1">
+                        ¿¿Cuáles son las categorías de productos que ofrecen en la compra online?
+                    </button>
+                </h2>
+            </div>
+            <div id="respuesta-1" class="collapse show" aria-labelledby="pregunta-1" data-parent="#preguntas-accordion">
+                <div class="card-body">
+                En nuestra tienda online, ofrecemos tres categorías de productos relacionados con mascotas: medicina, alimentos y accesorios. Puedes encontrar una amplia variedad de productos de alta calidad en cada una de estas categorías para satisfacer las necesidades de tus gatos y perros.
                 </div>
             </div>
-            <div class="col-12 mb-n2 p-0">
-                <iframe style="width: 100%; height: 500px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+
+        <div class="card">
+            <div class="card-header" id="pregunta-2">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#respuesta-2" aria-expanded="false" aria-controls="respuesta-2">
+                        ¿Cuáles son los métodos de pago aceptados para las donaciones?
+                    </button>
+                </h2>
+            </div>
+            <div id="respuesta-2" class="collapse" aria-labelledby="pregunta-2" data-parent="#preguntas-accordion">
+                <div class="card-body">
+                Aceptamos pagos de donaciones tanto a través de Yape como con tarjeta de débito. Si deseas realizar una donación para apoyar nuestra labor en la protección y cuidado de las mascotas, puedes seleccionar la opción de pago que más te convenga al realizar tu contribución.
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header" id="pregunta-3">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#respuesta-3" aria-expanded="false" aria-controls="respuesta-3">
+                        ¿Cómo puedo adoptar una mascota a través de su negocio?
+                    </button>
+                </h2>
+            </div>
+            <div id="respuesta-3" class="collapse" aria-labelledby="pregunta-3" data-parent="#preguntas-accordion">
+                <div class="card-body">
+                Nos complace que estés interesado en adoptar una mascota. Para iniciar el proceso de adopción, te pedimos que llenes un formulario de adopción en nuestro sitio web. Este formulario nos ayudará a conocer tus preferencias y asegurarnos de encontrar la mascota adecuada para ti. Después de recibir tu formulario, nos pondremos en contacto contigo para continuar con el proceso de adopción.
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header" id="pregunta-4">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#respuesta-4" aria-expanded="false" aria-controls="respuesta-4">
+                        ¿Cuáles son los beneficios de comprar productos para mascotas en Abrazo Peludo?
+                    </button>
+                </h2>
+            </div>
+            <div id="respuesta-4" class="collapse" aria-labelledby="pregunta-4" data-parent="#preguntas-accordion">
+                <div class="card-body">
+                Al comprar en Abrazo Peludo, te garantizamos productos de alta calidad para tus mascotas. Nuestros productos son cuidadosamente seleccionados para brindarles una alimentación adecuada, medicamentos seguros y accesorios funcionales. Además, al realizar una compra, estarás apoyando nuestra labor en la protección y bienestar de los animales.
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header" id="pregunta-5">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#respuesta-5" aria-expanded="false" aria-controls="respuesta-5">
+                        ¿Cómo puedo realizar un seguimiento de mi pedido realizado en línea?
+                    </button>
+                </h2>
+            </div>
+            <div id="respuesta-5" class="collapse" aria-labelledby="pregunta-5" data-parent="#preguntas-accordion">
+                <div class="card-body">
+                Después de realizar tu pedido, recibirás un correo electrónico de confirmación con los detalles de tu compra. También proporcionaremos un número de seguimiento que podrás utilizar para rastrear el estado de tu pedido. Si tienes alguna pregunta adicional sobre tu pedido, nuestro equipo de atención al cliente estará encantado de ayudarte.
+                </div>
             </div>
         </div>
     </div>
-    <!-- Contact End -->
+    
+    <!-- SECCIÓN DE CONSULTAS -->
 
 
     <!-- Footer Start -->
