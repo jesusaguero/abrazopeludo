440b5d560d1435bea876cf757460ae19dc7fc7bd
jesusaguerob <jesus.aguero@tecsup.edu.pe>
2023-07-09 03:09:34 -0500

    FINAL

--- database/migrations/2014_10_12_000000_create_users_tables.php
+++ database/migrations/2014_10_12_000000_create_users_tables.php
@@ -23,8 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('users');
     }
 };
