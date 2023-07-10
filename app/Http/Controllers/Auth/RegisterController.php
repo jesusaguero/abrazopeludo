61810d7550ac5ae1c185c262319acb47d94fa2c2
jesusaguerob <jesus.aguero@tecsup.edu.pe>
2023-07-09 22:23:28 -0500

    CRUD

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,6 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'dni' => ['required', 'string', 'max:8'],
-            'celular' => ['required', 'string', 'max:9'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -71,8 +69,6 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'tipo' => $data['tipo'],
             'password' => Hash::make($data['password']),
-            'dni' => $data['dni'],
-            'celular' => $data['celular'],
         ]);
     }
 }
